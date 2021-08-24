e65d8d6ac646c2266d1381ffa2af78c7b542b0f0
K1r0GreyH4t <89238534+Kir0GreyHat@users.noreply.github.com>
2021-08-25 01:31:19 +0800

    Update nautoscan.sh

--- nautoscan.sh
+++ nautoscan.sh
@@ -9,55 +9,53 @@ echo "
 ██║░╚███║██║░░██║╚██████╔╝░░░██║░░░╚█████╔╝██████╔╝╚█████╔╝██║░░██║██║░╚███║
 ╚═╝░░╚══╝╚═╝░░╚═╝░╚═════╝░░░░╚═╝░░░░╚════╝░╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░╚══╝
 
-- Automatic nuclei scanner by K1r0GreyH4t@N3S
-- Find me at : https://dragonforce.io/members/kir0greyh4t.9811/
-
 ";
+
+printf "\e[1;37m Automatic nuclei scanner by K1r0GreyH4t@N3S\e[0m\n"
+printf "\e[1;37m Find me at : https://dragonforce.io/members/kir0greyh4t.9811\e[0m\n"
 
 nuclei --update-templates --silent
 
 read -p "Enter domain names seperated by 'space' : " input
-for i in ${input[@]}
+ for i in ${input[@]}
 do
 
-echo "
-.
-.
-.
-.
-scan started for $i
-" | nst 'ALERT' "Scan started for $i"
+nst 'ALERT' "Scan started for $i"
 
 mkdir $i 
 
-subfinder -d $i | httpx >> $i/subdomains.txt | nst 'ALERT' "Finding subdomains.."
+ if subfinder -d $i | httpx >> $i/subdomains.txt | nst 'ALERT' "Finding subdomains.."
+then
+echo "subdomains saved at $i/subdomains.txt." | cat $i/subdomains.txt | nst "Found subdomains of $i" --stdin
+fi
 
-echo "subdomains saved at $i/subdomains.txt." | nst 'ALERT' "subdomains saved."
+printf "\e[0m\e[1;33mNuclei is starting\e[0m\n"
 
-echo "Scan for CVES started." | nst 'ALERT' "Scan for CVES started..."
+sleep 4
+
+echo "scan for CVES started." | nst 'ALERT' "Scan for CVES started..."
 nuclei -l $i/subdomains.txt -t cves/ -o $i/cves.txt 
-echo "scan for CVES completed." | nst 'ALERT' "Scan for CVES completed..."
+echo "scan for CVES completed." | cat $i/cves.txt | nst "CVES:$i" --stdin
+
 echo "scan for default-logins started." | nst 'ALERT' "Scan for default-logins started..."
-nuclei -l $i/subdomains.txt -t default-logins/ -o $i/default-logins.txt 
-echo "scan for default-logins completed." | nst 'ALERT' "Scan for default-logins completed."
+nuclei -l $i/subdomains.txt --silent -t default-logins/ -o $i/default-logins.txt 
+echo "scan for default-logins completed." | cat $i/default-logins.txt | nst "default-logins:$i" --stdin
+
 echo "scan for exposures started." | nst 'ALERT' "Scan for exposures started..."
-nuclei -l $i/subdomains.txt -t exposures/ -o $i/exposures.txt
-echo "scan for exposures completed." | nst 'ALERT' "Scan for exposures completed."
+nuclei -l $i/subdomains.txt --silent -t exposures/ -o $i/exposures.txt
+echo "scan for exposures completed." | cat $i/exposures.txt | nst "exposures:$i" --stdin
+
 echo "scan for misconfiguration started." | nst 'ALERT' "Scan for misconfiguration started..."
-nuclei -l $i/subdomains.txt -t misconfiguration/ -o $i/misconfiguration.txt 
-echo "scan for misconfiguration completed" | nst 'ALERT' "Scan for misconfiguration completed."
+nuclei -l $i/subdomains.txt --silent -t misconfiguration/ -o $i/misconfiguration.txt 
+echo "scan for misconfiguration completed" | cat $i/misconfiguration.txt | nst "misconfiguration:$i" --stdin
+
 echo "scan for takeovers started." | nst 'ALERT' "Scan for takeovers started..."
-nuclei -l $i/subdomains.txt -t takeovers/ -o $i/takeovers.txt
-echo "scan for takeovers completed." | nst 'ALERT' "Scan for takeovers completed."
+nuclei -l $i/subdomains.txt --silent -t takeovers/ -o $i/takeovers.txt
+echo "scan for takeovers completed." | cat $i/takeovers.txt | nst "takeovers:$i" --stdin
+
 echo "scan for vulnerabilities started." | nst 'ALERT' "Scan for vulnerabilities started..."
-nuclei -l $i/subdomains.txt -t vulnerabilities/ -o $i/vulnerabilities.txt
-echo "scan for vulnerabilities completed." | nst 'ALERT' "Scan for vulnerabilities completed."
+nuclei -l $i/subdomains.txt --silent -t vulnerabilities/ -o $i/vulnerabilities.txt
+echo "scan for vulnerabilities completed." | cat $i/vulnerabilities.txt | nst "vulnerabilities:$i" --stdin
 
-echo "
-.
-.
-.
-Scan finished for $i
-" | nst 'ALERT' "Scan finished for $i"
+printf "\e[0m\e[1;33m The scan for $i is finished.\e[0m\n" | nst "The scan for $i is finished."
 done
-
