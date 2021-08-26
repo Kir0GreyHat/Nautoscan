0f0f548b84b44888e5beb1af46acee651487635f
K1r0GreyH4t <89238534+Kir0GreyHat@users.noreply.github.com>
2021-08-26 12:15:54 +0800

    Update nautoscan.sh

--- nautoscan.sh
+++ nautoscan.sh
@@ -11,13 +11,19 @@ echo "
 
 ";
 
-printf "\e[1;37m Automatic nuclei scanner by K1r0GreyH4t@N3S\e[0m\n"
-printf "\e[1;37m Find me at : https://dragonforce.io/members/kir0greyh4t.9811\e[0m\n"
+printf "\e[1;37m Automatic nuclei scanner by
+
+\e[1;31m@K1r0GreyH4t\e[0m\n
+\e[1;33m@lu21n\e[0m\n"
+
+printf "\e[0;34m Find us at:\e[0m\n" 
+printf "\e[1;37m https://dragonforce.io/members/kir0greyh4t.9811\e[0m\n"
+printf "\e[1;37m https://dragonforce.io/members/lu21n.9494\e[0m\n"
 
 nuclei --update-templates --silent
 
-read -p "Enter domain names seperated by 'space' : " input
- for i in ${input[@]}
+read -p "Enter file name : " input
+while IFS= read -r i;
 do
 
 nst 'ALERT' "Scan started for $i"
@@ -58,4 +64,4 @@ nuclei -l $i/subdomains.txt --silent -t vulnerabilities/ -o $i/vulnerabilities.t
 echo "scan for vulnerabilities completed." | cat $i/vulnerabilities.txt | nst "vulnerabilities:$i" --stdin
 
 printf "\e[0m\e[1;33m The scan for $i is finished.\e[0m\n" | nst "The scan for $i is finished."
-done
+done < "$input"
